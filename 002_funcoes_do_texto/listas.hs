3baf59ec4512570adb99053508501d587b09ef28
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2022-03-26 10:40:47 -0300

    trabalhando com funções

---
+++ 002_funcoes_do_texto/listas.hs
@@ -0,0 +1,36 @@
+{-
+    let lostNumbers = [4, 8, 15, 16, 23, 48]
+
+    CONCATENANDO LISTA
+        [1, 2, 3, 4] ++ [9, 10, 11, 12]
+        "Hello" ++ " " ++ "world"
+        ['w', 'o'] ++ ['o', 't']
+
+    ADICIONANDO ELEMENTO NA LISTA
+        'Q' : " GATINHA"
+        5 : [1, 2, 3, 4, 5]
+
+    ACESSANCO LISTA COM O ÍNDICE
+        "Steve Buscemi" !! 6
+        [9.4, 33.2, 96.2, 11.2, 23.25] !! 1 
+
+    COMPARAR LISTAS
+        [3, 2, 1] > [2, 1, 0] = True
+        [3, 4, 2] > [3, 4] = True
+
+    FUNÇÕES COM LISTAS
+        head [5, 4, 3, 2, 1] = 5
+        tail [5, 4, 3, 2, 1] = [4, 3, 2, 1]
+        last [5, 4, 3, 2, 1] = 1
+        init [5, 4, 3, 2, 1] = [5, 4, 3, 2]
+        length [5, 4, 3, 2, 1] = 5
+        null [] = True
+        reverse [5, 4, 3, 2, 1] = [1, 2, 3, 4, 5]
+        take 3 [5, 4, 3, 2, 1] = [5, 4, 3]
+        drop 3 [8, 4, 2, 1, 5, 6] = [1, 5, 6]
+        maximum [1, 9, 2, 3, 4] = 9
+        minimum [8, 4, 2, 1, 5, 6] = 1
+        sum [5, 2, 1, 6, 3, 2, 5, 7] = 31
+        product [6, 2, 1, 2] = 24
+        4 `elem` [3, 4, 5, 6] = True
+-}
