fdae7cef4d0e76acfc4096149b0cdd08cf43fd7b
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2022-04-06 17:47:43 -0300

    correção

--- 005_aperacoes_basicas/005_primeiro.hs
+++ 005_aperacoes_basicas/005_primeiro.hs
@@ -2,10 +2,5 @@ primeiros [x] = []
 primeiros (x:xs) = x : (primeiros xs)
 
 main = do
-    print $ primeiros [1] == []
-    print $ primeiros [1,2] == [1]
-    print $ primeiros [1,2,3,4] == [1,2,3]
-    print $ primeiros [1,2,3,4,5] == [1,2,3,4]
-    print $ primeiros [1,2,3,4,5,6] == [1,2,3,4,5]
-    print $ primeiros [1,2,3,4,5,6,7] == [1,2,3,4,5,6]
-    print $ primeiros [1,2,3,4,5,6,7,8] == [1,2,3,4,5,6,7]
+    a <- readLn :: IO [Int]
+    print $ primeiros a
