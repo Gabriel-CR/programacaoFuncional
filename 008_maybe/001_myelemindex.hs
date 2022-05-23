dc675e82731c3033f009eeb70b8cfa788264d6b2
Gabriel-CR <gabriel.cgcr.cgcr@gmail.com>
2022-05-23 12:41:42 -0300

    finalizado

--- 008_maybe/001_myelemindex.hs
+++ 008_maybe/001_myelemindex.hs
@@ -1,7 +1,11 @@
-myelemIndex a xs = 
+fn :: (Eq t1, Num t2) => t1 -> [t1] -> t2 -> Maybe t2
+fn _ [] _ = Nothing
+fn a (x:xs) i = if a == x then Just i else fn a xs (i+1)
+
+myelemIndex :: (Eq t1, Num t2) => t1 -> [t1] -> Maybe t2
+myelemIndex a xs = fn a xs 0
 
 main = do
-    print $ myelemIndex 4 [1,2,6,4,3] == Just 3
-    print $ myelemIndex 5 [1,2,6,4,3] == Nothing
-    print $ myelemIndex 4 [] == Nothing
-    print $ myelemIndex 1 [1,2,6,4,3] == Just 0
+    a <- readLn :: IO Int
+    b <- readLn :: IO [Int]
+    print $ myelemIndex a b
