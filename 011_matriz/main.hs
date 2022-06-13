e112369b95fe5826c7a1c1c619fcc57e729be918
Gabriel-CR <gabriel.cgcr.cgcr@gmail.com>
2022-06-13 14:26:53 -0300

    colocando tipos

--- 011_matriz/main.hs
+++ 011_matriz/main.hs
@@ -1,15 +1,19 @@
 import Control.Monad (replicateM)
 
+setL :: [a] -> Int -> a -> [a]
 setL xs pos value = take pos xs ++ [value] ++ drop (pos + 1) xs
 
+setM :: [[a]] -> Int -> Int -> a -> [[a]]
 setM xxs l c value = inicio ++ [meio] ++ fim
     where
         inicio = take l xxs
         meio = setL (xxs !! l) c value
         fim = drop (l + 1) xxs
 
+neibs :: (Num a1, Num a2) => (a1, a2) -> [(a1, a2)]
 neibs (l, c) = [(l - 1, c), (l + 1, c), (l, c - 1), (l, c + 1)]
 
+burn :: (Int, Int, [[Char]]) -> (Int, Int) -> (Int, Int, [[Char]])
 burn (nl, nc, mat) (l, c)
     | l < 0 || l >= nl || c < 0 || c >= nc = (nl, nc, mat)
     | mat !! l !! c /= '#' = (nl, nc, mat)
