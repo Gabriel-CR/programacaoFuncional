4edb287cf9e813c905a45271d5a11b0dd9b0da4b
Gabriel-CR <gabriel.cgcr.cgcr@gmail.com>
2022-07-07 16:58:37 -0300

    up

--- 004_aperacoes_basicas/015_swap.hs
+++ 004_aperacoes_basicas/015_swap.hs
@@ -1,11 +1,21 @@
-swap xs a b = if (a <= length xs) && (b <= length xs) then fn else xs
-    where fn = take a xs ++ [b'] ++ centro ++ [a'] ++ drop (b + 1) xs
-          a' = xs !! a
-          b' = xs !! b
-          centro = take (b - a - 1) (drop (a + 1) xs)
+fn :: [a] -> Int -> Int -> [a]
+fn xs i j = esq ++ [xs !! j] ++ mid ++ [xs !! i] ++ dir
+    where
+        dir = drop (j + 1) xs
+        esq = take i xs
+        mid = take (j - i - 1) (drop (i + 1) xs)
 
+swap :: [a] -> Int -> Int -> [a]
+swap xs i j
+    | i > length xs || j > length xs = xs
+    | null xs = []
+    | otherwise = fn xs i j
+
+
+main :: IO ()
 main = do
-    a <- readLn :: IO [Int]
-    b <- readLn :: IO Int
-    c <- readLn :: IO Int
-    print $ swap a b c
+    print $ null (swap [] 0 5)
+    print $ swap [1] 0 3            == [1]
+    print $ swap [1,3,4] 1 2        == [1,4,3]
+    print $ swap [1,2,3,4,5,6] 2 5  == [1,2,6,4,5,3]
+    print $ swap [5,6,7,8,9] 0 3    == [8,6,7,5,9]
--- 004_aperacoes_basicas/_015_swap.hs
+++
@@ -1,8 +0,0 @@
-swap xs i j = []
-
-main = do
-    print $ swap [] 0 5 == []
-    print $ swap [1] 0 3 -- == [1]
-    print $ swap [1,3,4] 1 2 -- == [1,4,3]
-    print $ swap [1,2,3,4,5,6] 2 5 -- == [1,2,6,4,5,3]
-    print $ swap [5,6,7,8,9] 0 3 -- == [8,6,7,5,9]
